8ffdd17e323611726a7f70e9b9af60a79e7b7bb1
Shun-Hsing (Earl) Ou <occqoo@gmail.com>
2014-01-21 14:47:15 +0800

    mswave feature extraction

---
+++ src/tool/mswave_feature.cpp
@@ -0,0 +1,52 @@
+#include <opencv2/opencv.hpp>
+using namespace cv;
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  if (argc != 3) {
+    printf("usage: %s <video> <output.txt>\n", argv[0]);
+    return -1;
+  }
+
+  VideoCapture cap(argv[1]);
+  BackgroundSubtractorMOG2 mog;
+
+
+  float h_step = 180.0f / 16;
+  float s_step = 256.0f / 4;
+  float v_step = 256.0f / 4;
+
+  FILE* ofile = fopen(argv[2], "w");
+  Mat frame;
+  while (cap.read(frame)) {
+    Mat mask;
+    mog(frame, mask);
+    mask = mask == 255;
+
+    Mat hsv;
+    cvtColor(frame, hsv, CV_BGR2HSV);
+    Mat feature(256, 1, CV_32FC1);
+    feature.setTo(0);
+    if (countNonZero(mask) >
+        frame.size().height * frame.size().width * 0.05) {
+      for (int y=0; y<hsv.size().height; ++y) {
+        uchar* ptr = hsv.ptr<uchar>(y);
+        for (int x=0; x<hsv.size().width; ++x, ptr+=3) {
+          if (mask.at<uchar>(y, x) == 0) continue;
+          int h = floor(ptr[0] / h_step);
+          int s = floor(ptr[1] / s_step);
+          int v = floor(ptr[2] / v_step);
+          int i = h + (s << 4) + (v << 6);
+          feature.at<float>(i)++;
+        }
+      }
+      normalize(feature, feature, 1, 0, NORM_L1);
+      for (int i=0; i<256; ++i) {
+        fprintf(ofile, "%f ", feature.at<float>(i));
+      }
+      fprintf(ofile, "\n");
+    }
+  }
+  fclose(ofile);
+}
