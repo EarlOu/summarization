cf76a0cd20bd98a6d0294719b9bcfe960f618e3b
Shun-Hsing (Earl) Ou <occqoo@gmail.com>
2014-01-29 10:32:49 +0800

    visual attention

---
+++ src/tool/va_summary.cpp
@@ -0,0 +1,54 @@
+#include "database/Dataset.hpp"
+
+#include <stdio.h>
+#include <deque>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  if (argc != 4) {
+    printf("usage: %s <dataset> <score_dir> <output.txt>\n", argv[0]);
+    return -1;
+  }
+
+  Dataset set(argv[1], argv[1]);
+  vector<VideoInfo> & info = set.getVideoInfo();
+  FILE* ofile = fopen(argv[3], "w");
+
+  //double threshold = 0.57; // bl7f
+  //double threshold = 0.4; // office
+  double threshold = 0.57; // bl7f
+  int win = 31;
+
+  for (size_t vid=0; vid<info.size(); ++vid) {
+    char buf[128];
+    sprintf(buf, "%s/score_%d.txt", argv[2], (int) vid);
+    FILE* ifile = fopen(buf, "r");
+
+    double score = 0.0;
+    int idx = 0;
+    deque<double> scores(win);
+    for (int i=0; i<win; ++i) {
+      scores[i] = 0;
+    }
+    while (fscanf(ifile, "%lf", &score) == 1) {
+      scores.pop_front();
+      scores.push_back(score);
+
+      if (scores[win/2] > threshold) {
+        bool peak = true;
+        for (int i=0; i<win; ++i) {
+          if (scores[win/2] < scores[i]) {
+            peak = false;
+            break;
+          }
+        }
+        if (peak && idx > win/2) {
+          fprintf(ofile, "%d %d\n", (int) vid, idx - win/2 + info[vid].offset);
+        }
+      }
+      idx++;
+    }
+    fclose(ifile);
+  }
+  fclose(ofile);
+}
