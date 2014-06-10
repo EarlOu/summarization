03088ae7f365a44496f8e84598add8fc8dffd408
Shun-Hsing (Earl) Ou <occqoo@gmail.com>
2014-06-10 00:54:26 +0000

    recv msg and start sensor

--- inc/rpi/common/Common.h
+++ inc/rpi/common/Common.h
@@ -5,4 +5,7 @@ const int PORT = 1234;
 const int PORT_FEATURE = 1235;
 const int PORT_VIDEO = 1236;
 
+const char MSG_START = 0;
+const char MSG_STOP = 1;
+
 #endif
--- src/rpi/sensor/rpi_sensor.cpp
+++ src/rpi/sensor/rpi_sensor.cpp
@@ -1,83 +1,124 @@
+#include "gmm/Sensor.h"
+#include "gmm/Sender.h"
 #include "rpi/common/Common.h"
 #include "rpi/common/NetUtil.h"
 #include "rpi/common/ConcurrentQueue.h"
+#include "rpi/rpi_opencv/RpiVideoCapture.h"
+#include "rpi/rpi_opencv/RpiVideoWriter.h"
 
 #include <arpa/inet.h>
+#include <fcntl.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
+#include <unistd.h>
 
 #include <thread>
 #include <utility>
 
+#include <opencv2/opencv.hpp>
+using namespace cv;
+
 typedef std::pair<int, int> Shot;
 
-// #include "gmm/Sensor.h"
-// #include "gmm/Sender.h"
-//
-// #include "rpi/rpi_opencv/RpiVideoCapture.h"
-// #include "rpi/rpi_opencv/RpiVideoWriter.h"
-//
-// #include <fcntl.h>
-// #include <stdio.h>
-// #include <sys/time.h>
-//
-// #include <opencv2/opencv.hpp>
-// using namespace cv;
-//
-// class RpiSender: public Sender {
-// public:
-//     RpiSender() {}
-//
-//     virtual void sendFrame(InputArray frame, size_t time, int idx) {}
-//
-//     virtual void sendFeature(InputArray iFeature, double score, size_t time, int idx) {}
-// private:
-// };
-//
-// static size_t gettime() {
-//     struct timeval tv;
-//     gettimeofday(&tv, NULL);
-//     return tv.tv_sec * 1000 + tv.tv_usec / 1000;
-// }
-
-class ShotInfoSender {
+class RpiSender: public Sender {
 public:
-    ShotInfoSender(int sockfd): _sockfd(sockfd), _thread(run, sockfd, &_queue) {}
+    RpiSender() {}
 
-    void sendShotInfo(Shot& s) {
-        _queue.push(s);
-    }
-    void end() {
-        _queue.push(Shot(0, 0));
-        _thread.join();
-    }
+    virtual void sendFrame(InputArray frame, size_t time, int idx) {}
+
+    virtual void sendFeature(InputArray iFeature, double score, size_t time, int idx) {}
 private:
-    ConcurrentQueue<Shot> _queue;
-    int _sockfd;
-    std::thread _thread;
-
-    static void run(int fd, ConcurrentQueue<Shot>* q) {
-        Shot s;
-        int32_t buf[2];
-        while (true) {
-            s = q->pop();
-            if (s.first == 0 && s.second == 0) break;
-            buf[0] = s.first;
-            buf[1] = s.second;
-            if (sendall(fd, (char*) buf, 2 * sizeof(int32_t)) < 0) {
-                perror("Failed to send segment information");
-                exit(EXIT_FAILURE);
-            }
-        }
-    }
 };
+
+static size_t gettime() {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec * 1000 + tv.tv_usec / 1000;
+}
+
+// class ShotInfoSender {
+// public:
+//     ShotInfoSender(int sockfd): _sockfd(sockfd), _thread(run, sockfd, &_queue) {}
+//
+//     void sendShotInfo(Shot& s) {
+//         _queue.push(s);
+//     }
+//     void end() {
+//         _queue.push(Shot(0, 0));
+//         _thread.join();
+//     }
+// private:
+//     ConcurrentQueue<Shot> _queue;
+//     int _sockfd;
+//     std::thread _thread;
+//
+//     static void run(int fd, ConcurrentQueue<Shot>* q) {
+//         Shot s;
+//         int32_t buf[2];
+//         while (true) {
+//             s = q->pop();
+//             if (s.first == 0 && s.second == 0) break;
+//             buf[0] = s.first;
+//             buf[1] = s.second;
+//             if (sendall(fd, (char*) buf, 2 * sizeof(int32_t)) < 0) {
+//                 perror("Failed to send segment information");
+//                 exit(EXIT_FAILURE);
+//             }
+//         }
+//     }
+// };
+
+void run_sensor(RpiVideoCapture* cap, RpiVideoWriter* writer, bool* stop_flag) {
+    RpiSender sender;
+    Sensor sensor(&sender, false);
+
+    list<ReceivedFeature> features;
+    Mat frame;
+    int idx = 0;
+    while (!(*stop_flag) && cap->read(frame)) {
+        sensor.next(idx, frame, gettime(), features);
+        idx++;
+    }
+    sensor.finish();
+    cap->release();
+    writer->release();
+}
+
+char recv_msg(int msg_fd) {
+    char msg;
+    int n;
+    if ((n = recv(msg_fd, &msg, 1, 0)) <=0) {
+        if (n == 0) {
+            printf("Connction Break");
+        } else {
+            perror("Failed to recv msg");
+        }
+        exit(EXIT_FAILURE);
+    }
+    return msg;
+}
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         printf("usage: %s <server ip>\n", argv[0]);
         return -1;
     }
+
+    const int width = 320;
+    const int height = 240;
+
+    RpiVideoCapture& cap = RpiVideoCapture::getInstance();
+    cap.init(width, height);
+
+    int encoder_pipe[2];
+    if (pipe(encoder_pipe) < 0) {
+        perror("Failed to create encoder pipe");
+        exit(EXIT_FAILURE);
+    }
+    RpiVideoWriter& writer = RpiVideoWriter::getInstance();
+    writer.init(width, height, 30, encoder_pipe[1]);
 
     sockaddr_in addr;
     if (inet_aton(argv[1], (struct in_addr*) &addr) < 0) {
@@ -94,42 +135,15 @@ int main(int argc, char *argv[]) {
     int fd_video = listen_single_connect(PORT_VIDEO);
     printf("Successfully create video channel, fd=%d\n", fd_video);
 
-    ShotInfoSender shotInfoSender(fd_msg);
+    char msg = recv_msg(fd_msg);
+    assert(msg == MSG_START);
 
-    Shot s;
-    while (scanf("%d %d\n", &(s.first), &(s.second)) == 2) {
-        shotInfoSender.sendShotInfo(s);
-    }
-    shotInfoSender.end();
+    bool stop_sensor_flag = false;
+    std::thread sensor_thread(run_sensor, &cap, &writer, &stop_sensor_flag);
 
-//     const int width = 320;
-//     const int height = 240;
-//
-//     if (argc != 2) {
-//         printf("usage: %s <output_file>\n", argv[0]);
-//         return -1;
-//     }
-//
-//     RpiVideoCapture& cap = RpiVideoCapture::getInstance();
-//     cap.init(width, height);
-//
-//     int o_fd = open(argv[1], O_WRONLY | O_CREAT,  S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-//     if (o_fd < 0) {
-//         perror("Failed to open output file");
-//         exit(EXIT_FAILURE);
-//     }
-//     RpiVideoWriter& writer = RpiVideoWriter::getInstance();
-//     writer.init(width, height, 30, o_fd);
-//
-//     RpiSender sender;
-//     Sensor sensor(&sender, false);
-//
-//     list<ReceivedFeature> features;
-//     Mat frame;
-//     int idx = 0;
-//     while (cap.read(frame)) {
-//         sensor.next(idx, frame, gettime(), features);
-//         idx++;
-//     }
-//     sensor.finish();
+    msg = recv_msg(fd_msg);
+    assert(msg == MSG_STOP);
+
+    stop_sensor_flag = true;
+    sensor_thread.join();
 }
